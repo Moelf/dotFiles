3dd0e51be1e54af6ff0c3cbffd23203c62129b10
Moelf <jerryling315@gmail.com>
2018-07-27 15:10:26 -0700

    Add uaf numbering and sync NAS

--- imgcat.sh
+++ imgcat.sh
@@ -4,5 +4,5 @@ dir="$HOME/imgdump"
 rm -f $dir/*.png
 scp -q -r jling@uaf-8.t2.ucsd.edu:~/public_html/imgcat/. $dir
 # feh -y 300 -E 300 --thumbnails -d -S filename "$dir"
-feh -S filename -x -g 1000x750 --zoom fill "$dir"
+feh -d --draw-tinted -e "hack/19" -S filename -x -g 900x670 --zoom fill "$dir"
 # feh -B black -Z --scale-down "$dir"
