1fe239cf0b085fab6d889243921b0b851730f8e9
Moelf <jerryling315@gmail.com>
2021-05-28 17:23:13 -0400

    going to stow

--- home.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-swaymsg output DP-2 position 0 0
-swaymsg output DP-2 scale 1
-swaymsg output eDP-1 position 400 2160
--- office.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-swaymsg output eDP-1 position 0 1200
-swaymsg output DP-5 position 200 0
-swaymsg output DP-5 scale 1
