2a0be97562d613bc387a6c232ad662c80b8b6b63
Jerry Ling <proton@jling.dev>
2022-01-02 20:44:46 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-sudo pacman -Syu fisher fish neovim stow
+sudo pacman -Syu wl-clipboard xsel git fisher fish neovim stow fzf
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 fisher install PatrickF1/fzf.fish
 fisher install jethrokuan/z
+fisher install jomik/fish-gruvbox
