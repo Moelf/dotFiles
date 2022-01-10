18c64e37c11499fca0367fc69bbc0168f30f3fa4
Jerry Ling <proton@jling.dev>
2022-01-10 07:42:04 -0500

    bump

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -3,5 +3,10 @@ using OhMyREPL
 colorscheme!("GruvboxDark")
 import Base64:base64decode
 mega(s::String) = (String ∘ base64decode)(s)
-mega() = mega(clipboard())
+function mega()
+    origin = clipboard()
+    url = mega(origin)
+    clipboard(url)
+    run(`xdg-open $url`)
+end
 using Revise
