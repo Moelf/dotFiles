c3f547390b15bcc3c9604a82cb49b54a6459c23b
Moelf <proton@jling.dev>
2024-04-30 16:39:01 -0400

    neofetch -> fastfetch

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/fish
-sudo pacman -Syu wl-clipboard git fisher fish neovim stow fzf ripgrep wofi otf-font-awesome pamixer pipewire pipewire-pulse alacritty playerctl
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+sudo pacman -Syu wl-clipboard git fisher fish neovim stow fzf ripgrep wofi otf-font-awesome pamixer pipewire pipewire-pulse alacritty playerctl fastfetch
 
 fisher install PatrickF1/fzf.fish
 fisher install jethrokuan/z
